c1dc5ef79fee2ac697d15c881864aae9a54a0677
Daaren <killxcrafter@gmail.com>
2022-02-03 20:14:13 +0100

    Revert "Updates"
    This reverts commit b37b553c

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.redd.it/sp7o79ing0c71.png" className="App-logo" alt="logo" />
+        <p>
+          nice <code>coq</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://daarenx.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DaarenSite
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
